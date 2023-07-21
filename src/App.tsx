7abab183a785dcf158a48e21ee2a02a3e5fc069f
sivanesansagadhevan <126884011+sivanesansagadhevan@users.noreply.github.com>
2023-07-21 16:27:04 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,15 @@
+// App.tsx
+import React from "react";
+import Login from "./Login";
+import "./styles.css"; // Import the styles.css file
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
 
